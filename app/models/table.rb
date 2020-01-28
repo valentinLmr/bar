555761890476f0ced373c7e216eed7b154360a2b
valentin lmr <valentinlmr28@gmail.com>
2020-01-28 11:03:26 +0100

    destroy

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,4 +1,11 @@
 class Table < ApplicationRecord
   belongs_to :restaurant
   validates :number, presence: true
+
+  # after_create :generate_code
+
+  # def generate_code
+  #   self.code = SecureRandom.hex
+  #   save
+  # end
 end
